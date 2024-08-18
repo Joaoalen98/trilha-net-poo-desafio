dac32850570c86831cde59c424fc4040bf01fbad
João Alencar <joao.alencar19982@gmail.com>
2024-08-17 22:23:29 -0300

    Implementado métodos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smarthpone Nokia");
+Smartphone nokia = new Nokia("11900008888");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smarthpone Nokia");
+Smartphone iPhone = new Iphone("11900008888");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
